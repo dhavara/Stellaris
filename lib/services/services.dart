dcdc69a1ab86f324c04c9b7d699025cc00da4a60
Enrico <enricowakiman9@gmail.com>
2022-12-25 13:55:51 +0700

    api???

---
+++ lib/services/services.dart
@@ -0,0 +1,9 @@
+import 'dart:math';
+
+import 'package:crypto/crypto.dart';
+import 'package:dietaryminder/shared/shared.dart';
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+part 'fatsecretservice.dart';
---
+++ lib/shared/const.dart
@@ -0,0 +1,7 @@
+part of 'shared.dart';
+
+class Const {
+  static String baseUrl = 'platform.fatsecret.com';
+  static String clientId = '932f9479ff9b439291a6b80eae6e13e9';
+  static String clientSecret = '5a69722766bd4cefa5438bd2c00d340c';
+}
