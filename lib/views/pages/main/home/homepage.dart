5f0feacb1ced5de984539e87fa605d7c4207621d
vannessza <vanthony@student.ciputra.ac.id>
2023-01-02 23:33:08 +0700

    change logout

--- lib/views/pages/main/home/homepage.dart
+++ lib/views/pages/main/home/homepage.dart
@@ -33,23 +33,6 @@ class _HomePageState extends State<HomePage> {
             resizeToAvoidBottomInset: false,
             appBar: AppBar(
               title: const Text('Home'),
-              actions: [
-                IconButton(
-                    icon: const Icon(Icons.logout),
-                    color: Colors.white,
-                    onPressed: () async {
-                      SharedPreferences prefs =
-                          await SharedPreferences.getInstance();
-                      prefs.remove('id');
-                      if (!mounted) return;
-                      BlocProvider.of<UserCubit>(context).logout();
-                      Navigator.pushAndRemoveUntil<dynamic>(
-                          context,
-                          MaterialPageRoute<dynamic>(
-                              builder: (context) => const LoginPage()),
-                          (route) => false);
-                    }),
-              ],
               backgroundColor: const Color(0xFF91C788),
             ),
             body: Container(
--- lib/views/pages/profile/userprofile.dart
+++ lib/views/pages/profile/userprofile.dart
@@ -12,7 +12,24 @@ class _UserProfileState extends State<UserProfile> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Profile'),
+        title: const Text('Home'),
+        actions: [
+          IconButton(
+              icon: const Icon(Icons.logout),
+              color: Colors.white,
+              onPressed: () async {
+                SharedPreferences prefs = await SharedPreferences.getInstance();
+                prefs.remove('id');
+                if (!mounted) return;
+                BlocProvider.of<UserCubit>(context).logout();
+                Navigator.pushAndRemoveUntil<dynamic>(
+                    context,
+                    MaterialPageRoute<dynamic>(
+                        builder: (context) => const LoginPage()),
+                    (route) => false);
+              }),
+        ],
+        backgroundColor: const Color(0xFF91C788),
       ),
     );
   }
